1dc418f029eb7c23c4834b389bfaf67fc1d34835
gilberto <ant.gilbertosantos@gmail.com>
2024-02-23 12:08:10 -0300

    adicinando stg_clientes

---
+++ banvic_data/models/staging/stg_clientes.sql
@@ -0,0 +1,20 @@
+with
+    stg_clientes as (
+        select 
+          cod_cliente
+          , primeiro_nome
+          , ultimo_nome
+          , email
+          , tipo_cliente
+          , cast(parse_timestamp('%Y-%m-%d %H:%M:%S UTC', data_inclusao) as date) as data_inclusao
+          , cpfcnpj
+          , data_nascimento
+          , endereco
+          , cep
+        from {{ source('dev_gilberto', 'clientes') }} 
+    )
+
+select *
+from stg_clientes
+
+

8ea6ffde5c93026f1ce33e014f4bab2f246df959
gilberto <ant.gilbertosantos@gmail.com>
2024-02-26 14:18:41 -0300

    alterando nome das colunas cliente e transacao

--- banvic_data/models/staging/stg_transacoes.sql
+++ banvic_data/models/staging/stg_transacoes.sql
@@ -1,8 +1,8 @@
 with 
     stg_transacoes as (
         select 
-          cod_transacao as id_transacoes
-          , num_conta
+          cod_transacao as transacao_id
+          , num_conta as cliente_id
           , cast(parse_timestamp('%Y-%m-%d %H:%M:%S UTC', data_transacao) as date) as data_transacao
           , nome_transacao
           , valor_transacao

66cc6b2a172843749d2ce65d3b7dde3ceb544ff6
gilberto <ant.gilbertosantos@gmail.com>
2024-02-26 12:38:31 -0300

    renomeando coluna

--- banvic_data/models/staging/stg_colaborador_agencia.sql
+++ banvic_data/models/staging/stg_colaborador_agencia.sql
@@ -1,8 +1,8 @@
 with 
     stg_colaborador_agencia as (
         select 
-          cod_colaborador as id_colaborador
-          , cod_agencia as id_agencia
+          cod_colaborador as colaborador_id
+          , cod_agencia as agencia_id
         from {{ source('dev_gilberto', 'colaborador_agencia') }} 
     )
 select *

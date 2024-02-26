ebf99c0b8fe350d27e5a1293ba928ed3c5a3dcb9
gilberto <ant.gilbertosantos@gmail.com>
2024-02-26 12:37:38 -0300

    removendo colunas desnecessarias

--- banvic_data/models/staging/stg_colaboradores.sql
+++ banvic_data/models/staging/stg_colaboradores.sql
@@ -1,14 +1,9 @@
 with
     stg_colaboradores as (
         select 
-          cod_colaborador
-          , primeiro_nome
-          , ultimo_nome
-          , email
-          , cpf
-          , data_nascimento
-          , endereco
-          , cep
+          cod_colaborador as colaborador_id
+          , cast(primeiro_nome || ' ' || ultimo_nome as string) as nome_colaborador
+          , regexp_extract(endereco, r' / ([A-Z]{2})$') as estado
         from {{ source('dev_gilberto', 'colaboradores') }} 
     )
 select *

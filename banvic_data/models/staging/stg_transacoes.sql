4d1832eb9052dc7066691d78dd862f85c4011f6e
gilberto <ant.gilbertosantos@gmail.com>
2024-02-26 16:22:34 -0300

    alterando o formato de data

--- banvic_data/models/staging/stg_transacoes.sql
+++ banvic_data/models/staging/stg_transacoes.sql
@@ -3,8 +3,7 @@ with
         select 
           cod_transacao as transacao_id
           , num_conta as cliente_id
-          , cast(parse_timestamp('%Y-%m-%d %H:%M:%S UTC', data_transacao) as date) as data_transacao
-          , nome_transacao
+          , date(data_transacao) as data_transacao          , nome_transacao
           , valor_transacao
         from {{ source('dev_gilberto', 'transacoes') }}
     )

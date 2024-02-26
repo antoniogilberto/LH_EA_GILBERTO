a7e62cf72efcd3abb3765ea6155c458114f595b5
gilberto <ant.gilbertosantos@gmail.com>
2024-02-26 14:15:23 -0300

    alterando nome das colunas

--- banvic_data/models/staging/stg_propostas_credito.sql
+++ banvic_data/models/staging/stg_propostas_credito.sql
@@ -2,16 +2,16 @@ with
     stg_propostas_credito
      as (
         select 
-          cod_proposta as id_proposta
-          , cod_cliente as id_cliente
-          , cod_colaborador as id_colaborador
+          cod_proposta as proposta_id
+          , cod_cliente as cliente_id
+          , cod_colaborador as colaborador_id
           , cast(parse_timestamp('%Y-%m-%d %H:%M:%S UTC', data_entrada_proposta) as date) as data_entrada_proposta
           , taxa_juros_mensal
           , valor_proposta
           , valor_financiamento
           , valor_entrada
           , valor_prestacao
-          , quantidade_parcelas
+          , quantidade_parcelas as qtd_parcelas
           , carencia
           , status_proposta
         from {{ source('dev_gilberto', 'propostas_credito') }}

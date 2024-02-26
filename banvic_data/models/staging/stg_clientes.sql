5202cc33fc588e3fb27d80d015d0e16a7c7ded28
gilberto <ant.gilbertosantos@gmail.com>
2024-02-26 12:36:30 -0300

    adicionando coluna estado_cliente

--- banvic_data/models/staging/stg_clientes.sql
+++ banvic_data/models/staging/stg_clientes.sql
@@ -1,16 +1,14 @@
 with
     stg_clientes as (
         select 
-          cod_cliente as id_cliente
-          , primeiro_nome
-          , ultimo_nome
+          cast(cod_cliente as int) as cliente_id
+          , cast(primeiro_nome || ' ' || ultimo_nome as string) as nome_cliente
           , email
           , tipo_cliente
           , cast(parse_timestamp('%Y-%m-%d %H:%M:%S UTC', data_inclusao) as date) as data_inclusao
           , cpfcnpj
-          , data_nascimento
-          , endereco
-          , cep
+          , date_diff(current_date(), data_nascimento, year) as idade
+          , regexp_extract(endereco, r' / ([A-Z]{2})$') as estado
         from {{ source('dev_gilberto', 'clientes') }} 
     )
 select *

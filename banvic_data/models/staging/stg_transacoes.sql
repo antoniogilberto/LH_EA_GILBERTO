17bfda80cbac0ce0a40fb0964dd9cd61103ea60a
gilberto <ant.gilbertosantos@gmail.com>
2024-02-27 00:02:55 -0300

    enviando arquivo pbix

--- banvic_data/models/staging/stg_transacoes.sql
+++ banvic_data/models/staging/stg_transacoes.sql
@@ -3,7 +3,8 @@ with
         select 
           cod_transacao as transacao_id
           , num_conta as cliente_id
-          , date(data_transacao) as data_transacao          , nome_transacao
+          , date(data_transacao) as data_transacao
+          , nome_transacao
           , valor_transacao
         from {{ source('dev_gilberto', 'transacoes') }}
     )
